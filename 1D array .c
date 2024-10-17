88deefb4860443df541ab241816dfdccf6c98af1
den-g254 <denzelgitonga58@gmail.com>
2024-10-17 10:38:12 +0300

    Add files via upload

---
+++ 1D array .c
@@ -0,0 +1,22 @@
+//C arrays- 1D array
+
+
+/*
+Author:Mugendi Denzel Gitonga
+Reg No:CT101/G/24518/24
+Date:17/10/2024
+*/
+
+#include<stdio.h>
+
+int main(){
+	
+   int marks[5]= {50,60,70,80,2};
+   int i;
+   for(i=0;i<5;i++){
+	   printf("Marks[%d]=%d \n",i,marks[i]);
+   }
+   return 0;
+   
+}
+	
