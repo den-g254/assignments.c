d0de457c85d1f19ac10a10813fc41d1b2d70b24d
den-g254 <denzelgitonga58@gmail.com>
2024-10-25 07:10:57 +0300

    Add files via upload

---
+++ struct book.c
@@ -0,0 +1,34 @@
+//c structure
+/*program for prompt book
+author:vienna kimani
+reg:CT102/G/24668/24
+Date:22/10/2024
+*/
+#include<stdio.h>
+#include<string.h>//stcrpy
+struct book
+{
+	char title[30];
+	char author[30];
+	int publicationyear;
+	char ISBN[13];
+	float price;
+	
+} book;
+
+int main()
+{
+	//struct book;
+	
+	strcpy(book.title,"introduction to programming");
+	strcpy(book.author,"john smith");
+	book.publicationyear= 2022;
+	strcpy(book.ISBN,"978013110367");
+	book.price = 49.99;
+	
+	printf("title:%s \n",book.title);
+	printf("author:%s \n",book.author);
+	printf("publication year:%d \n",book.publicationyear);
+	printf("ISBN:%s \n",book.ISBN);
+	printf("price:%.2f \n",book.price);
+}
